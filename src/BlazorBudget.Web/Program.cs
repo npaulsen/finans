3cec578e1e36bbfa5b80842f0c021a20ff26bdd6
Niklas Paulsen <npau@informatik.uni-kiel.de>
2023-07-18 19:27:52 +0000

    Using BlazorBootstrap now

--- src/BlazorBudget.Web/Program.cs
+++ src/BlazorBudget.Web/Program.cs
@@ -1,10 +1,12 @@
 using BlazorBudget.Web.Data;
+using BlazorBootstrap;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddBlazorBootstrap();
 builder.Services.AddSingleton<TransactionService>();
 builder.Services.AddSingleton<ClassificationService>();
 
